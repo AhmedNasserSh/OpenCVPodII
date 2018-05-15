b55f64e2a0e78709dbc65eb3a1fca7318b5a6481
Ahmed Nasser <ahmed.nasser2310@gmail.com>
2018-05-15 13:56:12 +0200

    Initial Commit

---
+++ OpenCVPod/OpenCVPod.h
@@ -0,0 +1,19 @@
+//
+//  OpenCVPod.h
+//  OpenCVPod
+//
+//  Created by AHMED NASSER on 5/15/18.
+//  Copyright © 2018 AHMED NASSER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenCVPod.
+FOUNDATION_EXPORT double OpenCVPodVersionNumber;
+
+//! Project version string for OpenCVPod.
+FOUNDATION_EXPORT const unsigned char OpenCVPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenCVPod/PublicHeader.h>
+
+
